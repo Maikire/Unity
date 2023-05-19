7c91ca09d1558def25d8ddde8735549995fe6e1d
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-19 19:37:12 +0800

    Update SkillSelectorType.cs

--- UnityFramework/A Simple Skills Framework/SkillSystem/Common/SkillSelectorType.cs
+++ UnityFramework/A Simple Skills Framework/SkillSystem/Common/SkillSelectorType.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine;
 
 namespace ARPGDemo.Skill
 {
     public enum SkillSelectorType
     {
+        [Tooltip("扇形/圆形")]
         Sector
         //Rectangle
     }
