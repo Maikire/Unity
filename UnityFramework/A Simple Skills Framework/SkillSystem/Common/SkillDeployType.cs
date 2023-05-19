53923e9cda19fb486f9b06cfe02800433153f2a9
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-19 19:36:51 +0800

    Update SkillDeployType.cs

--- UnityFramework/A Simple Skills Framework/SkillSystem/Common/SkillDeployType.cs
+++ UnityFramework/A Simple Skills Framework/SkillSystem/Common/SkillDeployType.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ARPGDemo.Skill
 {
     public enum SkillDeployType
     {
+        [Tooltip("指定目标")]
         Target,
+
+        [Tooltip("指定方向")]
         Direction,
-        Location
+
+        [Tooltip("指定位置")]
+        Location,
+
     }
 
 }
