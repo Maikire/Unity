64b85be9be1e85e162f710baa58cb88c3af5e18a
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-20 11:43:11 +0800

    Add files via upload

---
+++ UnityFramework/A simple ARPG skill framework/ImpactEffects/CostMPImpact.cs
@@ -0,0 +1,20 @@
+using ARPGDemo.Character;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARPGDemo.Skill
+{
+    /// <summary>
+    /// 消耗法力值
+    /// </summary>
+    public class CostMPImpact : IImpactEffect
+    {
+        public void Execute(SkillDeployer skillDeployer)
+        {
+            CharacterStatus status = skillDeployer.SkillData.Owner.GetComponent<CharacterStatus>();
+            status.MP -= skillDeployer.SkillData.CostMP;
+        }
+
+    }
+}
