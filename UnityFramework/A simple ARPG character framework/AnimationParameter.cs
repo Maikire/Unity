82faff7e5082d323f0201f40c8a77f7580208a97
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-20 11:37:35 +0800

    Add files via upload

---
+++ UnityFramework/A simple ARPG character framework/AnimationParameter.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace ARPGDemo.Character
+{
+    //可序列化，将当前对象“嵌入”到脚本后，可以在编辑器中显示属性
+    //对应的，这将生成一个对象，不需要手动 new AnimationParameter()
+    [System.Serializable]
+    /// <summary>
+    /// AnimationParameter
+    /// </summary>
+    public class AnimationParameter
+    {
+        [Tooltip("跑步")]
+        public string Run = "run";
+
+        [Tooltip("死亡")]
+        public string Death = "death";
+
+        [Tooltip("闲置")]
+        public string Idle = "idle";
+
+        [Tooltip("复活")]
+        public string Revive = "revive";
+
+    }
+}
