a0b74d869a7563c35ea7e06f7ae3c7f88edef0b2
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-20 11:40:05 +0800

    Add files via upload

---
+++ UnityFramework/A simple ARPG character framework/PlayerStatus.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace ARPGDemo.Character
+{
+    /// <summary>
+    /// PlayerStatus
+    /// </summary>
+    public class PlayerStatus : CharacterStatus
+    {
+        private Animator PlayerAnimator;
+
+        private void Awake()
+        {
+            PlayerAnimator = this.transform.GetComponentInChildren<Animator>();
+        }
+
+        ////隐藏方法
+        //private new void Start()
+        //{
+        //    base.Start();
+        //}
+
+        /// <summary>
+        /// 死亡
+        /// </summary>
+        public override void Die()
+        {
+            PlayerAnimator.SetBool(PlayerAnimationParameter.Death, true);
+        }
+
+
+    }
+}
