137f5c24dc8e1b8da1d0b8da444e763002e20237
Maikire <118192763+Maikire@users.noreply.github.com>
2023-05-20 17:18:13 +0800

    Add files via upload

---
+++ UnityFramework/A simple ARPG skill framework/Common/CharacterSkillSystemNPC.cs
@@ -0,0 +1,38 @@
+using AI.FSM;
+using Common;
+using UnityEngine;
+
+namespace ARPGDemo.Skill
+{
+    [RequireComponent(typeof(FSMBase))]
+    /// <summary>
+    /// 为NPC提供的技能系统
+    /// </summary>
+    public class CharacterSkillSystemNPC : CharacterSkillSystem
+    {
+        private FSMBase Fsm;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Fsm = this.GetComponent<FSMBase>();
+        }
+
+        /// <summary>
+        /// 使用随机技能
+        /// 为NPC提供
+        /// </summary>
+        public void UseRandomSkill()
+        {
+            //先选出可以生成的技能，然后在生成随机数
+            SkillData[] skillData = SkillManger.Skills.FindAll(s => SkillManger.PrepareSkill(s.SkillID) != null);
+            if (skillData != null && skillData.Length > 0)
+            {
+                int index = Random.Range(0, skillData.Length);
+                UseSkill(skillData[index].SkillID);
+            }
+        }
+
+
+    }
+}
---
+++ UnityFramework/A simple ARPG skill framework/Common/CharacterSkillSystemPlayer.cs
@@ -0,0 +1,49 @@
+using ARPGDemo.Character;
+using UnityEngine;
+
+namespace ARPGDemo.Skill
+{
+    [RequireComponent(typeof(CharacterInputController), typeof(CharacterSelected))]
+    /// <summary>
+    /// 为玩家提供的技能系统
+    /// </summary>
+    public class CharacterSkillSystemPlayer : CharacterSkillSystem
+    {
+        private CharacterSelected SelectCharacter;
+        private CharacterInputController InputController;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            SelectCharacter = this.GetComponent<CharacterSelected>();
+            InputController = GetComponent<CharacterInputController>();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            EventBehaviour.CancelAnim.AddListener(CanMove);
+        }
+
+        public override void UseSkill(int skillID, bool isBatter = false)
+        {
+            base.UseSkill(skillID, isBatter);
+
+            //禁止移动（攻击的优先级高于移动）
+            InputController.IsMove = false;
+
+            //选中目标
+            SelectCharacter.SelectTargets(Skill.AttackTargets);
+        }
+
+        /// <summary>
+        /// 使角色可以移动
+        /// </summary>
+        private void CanMove()
+        {
+            InputController.IsMove = true;
+        }
+
+
+    }
+}
