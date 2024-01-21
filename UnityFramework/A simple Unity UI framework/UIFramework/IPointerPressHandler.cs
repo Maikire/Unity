6a60a44251e2a5ee3248d62be4d73d2a3b481d0b
Maikire <118192763+Maikire@users.noreply.github.com>
2024-01-21 19:58:46 +0800

    Update IPointerPressHandler.cs

--- UnityFramework/A simple Unity UI framework/UIFramework/IPointerPressHandler.cs
+++ UnityFramework/A simple Unity UI framework/UIFramework/IPointerPressHandler.cs
@@ -8,7 +8,6 @@ namespace Default
         /// 长按按钮
         /// </summary>
         /// <param name="eventData">PointerEventData</param>
-        /// <param name="pressTime">按下按钮的时间</param>
         void OnPointerPress(PointerEventData eventData);
     }
 }
